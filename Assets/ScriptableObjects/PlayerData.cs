3fe5bcbf9de68d5751d4b82a262ab1edffa4cd39
Jorge_dany <spumasc11@gmail.com>
2024-10-08 03:05:54 -0600

    Bloque de daño y checkpoints

--- Assets/ScriptableObjects/PlayerData.cs
+++ Assets/ScriptableObjects/PlayerData.cs
@@ -7,14 +7,25 @@ public class PlayerData : ScriptableObject
     public bool hasLight = false;
     public bool hasLightv2 = false;
     public bool hasDoubleJump = false;
+    Vector3 checkpointposition = Vector3.zero;
+    public bool controlEnable=true;
     public void ResetPlayerData()
     {
         hasLight = false;
         hasLightv2 = false;
         hasDoubleJump = false;
-    }
+        controlEnable = true;
+}
     public bool HasAllRemembers()
     {
         return hasLight & hasLightv2 & hasDoubleJump;
+    }
+    public void SetCheckpointPosition(Vector3 newPos)
+    {
+        checkpointposition = newPos;
+    }
+    public Vector3 LastCheckpointPosition()
+    {
+        return checkpointposition;
     }
 }
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -12,27 +12,33 @@ public class PlayerController : MonoBehaviour
     public float cooldownDuracion = 3f;  // Duración del cooldown para volver a encender la luz
     public bool luzActiva = false;  // Para controlar si la luz está activada
     public LayerMask capaGround;  // Capa para detectar el suelo
-
-    [SerializeField] private float contadorTiempo = 0f;  // Contador para el tiempo que la luz está activada    
-
-    private bool puedeSaltarDoble = false;  // Controla si se puede hacer el segundo salto
     [SerializeField] private bool estaEnSuelo = false;  // Verifica si el jugador está en el suelo    
-    private bool enCooldown = false;  // Controla si está en cooldown
-    private float longitudRaycast = 1f;  // Longitud del Raycast
-    
+    [SerializeField] private float contadorTiempo = 0f;  // Contador para el tiempo que la luz está activada    
+    [SerializeField] private SpriteRenderer playerSprite;  // Contador para el tiempo que la luz está activada    
 
+    private bool puedeSaltarDoble = false;  // Controla si se puede hacer el segundo salto    
+    private bool enCooldown = false;  // Controla si está en cooldown
+    private float longitudRaycast = .7f;  // Longitud del Raycast
     private Rigidbody2D rb;
+    private bool isdead=false;
+    private Collider2D playerCollider;
+
+
     void Awake()
     {
         // Asigna el tag "Player"
         gameObject.tag = "Player";
         rb = GetComponent<Rigidbody2D>();
+        if (playerSprite == null) playerSprite = GetComponent<SpriteRenderer>();
+        playerCollider = GetComponent<Collider2D>();
         brillo.SetActive(false);
+        playerData.SetCheckpointPosition(transform.position);
     }
     void Update()
     {
-        if(playerData.hasLight) LightControl();
-        playerMovement();
+        if(playerData.hasLight && playerData.controlEnable) LightControl();
+        if(playerData.controlEnable) playerMovement();
+        if (isdead) ReturnToCheckpoint();
         DetectarSuelo();
     }
     void DetectarSuelo()
@@ -119,5 +125,31 @@ public class PlayerController : MonoBehaviour
         {
             brillo.transform.localScale = Vector3.one * 3;
         }
+    }
+    void ReturnToCheckpoint()
+    {
+        Vector2 destino = new Vector2(playerData.LastCheckpointPosition().x, playerData.LastCheckpointPosition().y);
+        float fspeed = 5f;
+        float distancia = Vector2.Distance(rb.position, destino);
+        if (distancia > .3f)
+        {
+            Vector2 direccion = (destino - rb.position).normalized;
+            rb.velocity = direccion * fspeed;
+        }
+        else
+        {
+            rb.velocity = Vector2.zero;
+            playerData.controlEnable = true;
+            isdead = false;
+            playerCollider.enabled = true;
+            playerSprite.color = new Color(playerSprite.color.r, playerSprite.color.g, playerSprite.color.b, 1f);
+        }
+    }
+    public void Dead()
+    {
+        playerData.controlEnable = false;
+        isdead = true;
+        playerCollider.enabled = false;
+        playerSprite.color = new Color(playerSprite.color.r, playerSprite.color.g, playerSprite.color.b, 0.1f);
     }
 }
