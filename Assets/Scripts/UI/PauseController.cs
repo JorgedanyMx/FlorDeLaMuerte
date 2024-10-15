fc6a3245b75e5b4399dbb84dbfc6791e7ae99aef
Jorge_dany <spumasc11@gmail.com>
2024-10-15 03:34:03 -0600

    Squashed commit of the following:
    commit 5343126380429527d4ea94cf71a4ce8de1a3452c
    Author: Andrés  Josué Castro Angulo <andres602@gmail.com>
    Date:   Mon Oct 14 20:10:46 2024 -0600
    
        Pause prefab

---
+++ Assets/Scripts/UI/PauseController.cs
@@ -0,0 +1,146 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseController : MonoBehaviour
+{
+    private GameObject pauseCanvasObj;
+    private GameObject pausePanelObj;
+    private GameObject SettingsPanelObj;
+    private GameObject ConfirmPanelObj;
+    private PauseState pauseState = PauseState.Resume;
+
+    private enum PauseState : int 
+    {
+        Pause = 0,
+        Settings = 1,
+        Confirm = 2,
+        Resume = 3
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        InitOBJ();
+        SetPanel();
+        pauseCanvasObj.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.P) )
+        {
+            if(pauseState == PauseState.Resume)PauseGame();
+            else if (pauseState == PauseState.Pause) ResumeGame();
+        }
+
+    }
+
+    public void OnClickContinue()
+    {
+        Debug.Log("Continue");
+        ResumeGame();
+    }
+
+    public void OnClickSettings()
+    {
+        Debug.Log("Settings");
+        SettingsGame();
+    }
+
+    public void OnClickHome()
+    {
+        Debug.Log("Home");
+        ConfirmGame();
+    }
+
+    public void OnClickYes()
+    {
+        Debug.Log("Yes");
+        ExitGame();
+    }
+
+    public void OnClickNo()
+    {
+        Debug.Log("No");
+        PauseGame();
+    }
+
+    public void OnSlideSFXVolume(Slider slider)
+    {
+        Debug.Log("SFX Volume: " + slider.value);
+    }
+
+    public void OnSlideMusicVolume(Slider slider)
+    {
+        Debug.Log("Music Volume: " + slider.value);
+    }
+
+    private void InitOBJ()
+    {
+        pauseCanvasObj = transform.Find("PauseCanvas").gameObject;
+        pausePanelObj = pauseCanvasObj.transform.Find("PausePanel").gameObject;
+        SettingsPanelObj = pauseCanvasObj.transform.Find("SettingsPanel").gameObject;
+        ConfirmPanelObj = pauseCanvasObj.transform.Find("ConfirmPanel").gameObject;
+    }
+
+    private void PauseGame()
+    {
+        pauseState = PauseState.Pause;
+        Time.timeScale = 0;
+        pauseCanvasObj.SetActive(true);
+        SetPanel();     
+    }
+
+    private void SettingsGame()
+    {
+        pauseState = PauseState.Settings;
+        SetPanel();
+    }
+
+    private void ConfirmGame()
+    {
+        pauseState = PauseState.Confirm;
+        SetPanel();
+    }
+    private void ResumeGame()
+    {
+        pauseState = PauseState.Resume;
+        SetPanel();
+        pauseCanvasObj.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    private void SetPanel()
+    {	
+        switch(pauseState)
+        {
+            case PauseState.Pause:
+                pausePanelObj.SetActive(true);
+                SettingsPanelObj.SetActive(false);
+                ConfirmPanelObj.SetActive(false);
+                break;
+            case PauseState.Settings:
+                pausePanelObj.SetActive(false);
+                SettingsPanelObj.SetActive(true);
+                ConfirmPanelObj.SetActive(false);
+                break;
+            case PauseState.Confirm:
+                pausePanelObj.SetActive(false);
+                SettingsPanelObj.SetActive(false);
+                ConfirmPanelObj.SetActive(true);
+                break;
+            case PauseState.Resume:
+                pausePanelObj.SetActive(false);
+                SettingsPanelObj.SetActive(false);
+                ConfirmPanelObj.SetActive(false);
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void ExitGame()
+    {
+        Application.Quit();
+    }
+}
